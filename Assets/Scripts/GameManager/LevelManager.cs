b07b0345e7b074d402e37fbc6a2ae34994099f36
VladimirSavostyanenko <vova.savostyanenko@gmail.com>
2022-03-27 21:15:24 +0300

    Add second person spawn

---
+++ Assets/Scripts/GameManager/LevelManager.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GameManager
+{
+    public class LevelManager : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private Button m_LeaveRoomButton;
+
+        [SerializeField]
+        private Transform m_SpawnPos;
+
+        [SerializeField]
+        private List<Transform> m_ListSpawnPosition;
+
+        [SerializeField]
+        private List<ExplodudesCrate> m_ExplodudesCrates;
+
+        private List<Character> m_Characters = new List<Character>();
+
+        private void Start()
+        {
+            m_LeaveRoomButton.onClick.AddListener(OnLeftRoom);
+
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                var player = PhotonNetwork.Instantiate("MinimalGridCharacter", m_ListSpawnPosition[0].position, Quaternion.identity);
+                m_Characters.Add(player.GetComponent<Character>());
+            }
+            else
+            {
+                var player = PhotonNetwork.Instantiate("MinimalGridCharacterSecond", m_ListSpawnPosition[1].position, Quaternion.identity);
+                m_Characters.Add(player.GetComponent<Character>());
+            }
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.LogFormat("Player{0} entered room", newPlayer.NickName);
+        }
+
+        public override void OnPlayerLeftRoom(Player player)
+        {
+            Debug.LogFormat("Player{0} entered room", player.NickName);
+        }
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        private void AddPlayer(Character character)
+        {
+            m_Characters.Add(character);
+        }
+    }
+}
--- Assets/Scripts/Lobby/LobbyController.cs
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -1,45 +1,60 @@
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LobbyController : MonoBehaviourPunCallbacks {
-  [SerializeField]
-  private Button _createRoom;
+public class LobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Button _createRoom;
 
-  [SerializeField]
-  private Button _joinGame;
+    [SerializeField]
+    private Button _joinGame;
 
-  private void Start() {
-    PhotonNetworkSetup();
-    SubscribeButton();
-  }
+    [SerializeField]
+    private TMP_Text m_Text;
 
-  public override void OnConnectedToMaster() {
-    Debug.Log("Conect");
-  }
+    private void Start()
+    {
+        PhotonNetworkSetup();
+        SubscribeButton();
+    }
 
-  public override void OnJoinedRoom() {
-    Debug.Log("JoinRoom");
-    PhotonNetwork.LoadLevel("GameScene");
-  }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conect");
+        m_Text.SetText("Conect");
+    }
 
-  private void SubscribeButton() {
-    _createRoom.onClick.AddListener(CreateRoom);
-    _joinGame.onClick.AddListener(JoinGame);
-  }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("JoinRoom");
+        PhotonNetwork.LoadLevel("GameScene");
+        m_Text.SetText("JoinRoom");
 
-  private void CreateRoom() {
-    PhotonNetwork.CreateRoom(null);
-  }
+    }
 
-  private void JoinGame() {
-    PhotonNetwork.JoinRandomRoom();
-  }
+    private void SubscribeButton()
+    {
+        _createRoom.onClick.AddListener(CreateRoom);
+        _joinGame.onClick.AddListener(JoinGame);
+    }
 
-  private static void PhotonNetworkSetup() {
-    PhotonNetwork.NickName = "Player" + Random.Range(1, 10);
-    PhotonNetwork.AutomaticallySyncScene = true;
-    PhotonNetwork.GameVersion = "1.0.0";
-    PhotonNetwork.ConnectUsingSettings();
-  }
+    private void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    private void JoinGame()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private static void PhotonNetworkSetup()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(1, 10);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1.0.0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
 }
